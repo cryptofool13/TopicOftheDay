fa1948e06ccbe12bf3c2157b92681c7eab764202
cryptofool13 <fender18295@gmail.com>
2020-08-04 16:46:40 -0400

    start testing app

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,11 @@
 import { JsonDB } from "node-json-db";
 import { Config } from "node-json-db/dist/lib/JsonDBConfig";
+import  request from 'supertest'
 
+import {app} from './app'
+import {db} from './db'
 import { randomIndex, Topic } from "./utils";
 
-const db = new JsonDB(new Config("TEST_topicDB", true, true, "/"));
 
 const DEV_TOPICS: Topic[] = [
   {
@@ -12,12 +14,32 @@ const DEV_TOPICS: Topic[] = [
     timesReturned: 0,
     id: 0,
   },
+  {
+    name: "N-Cycle app",
+    description: "finish the app you just started last month",
+    timesReturned: 0,
+    id: 1,
+  },
+  {
+    name: "japanese",
+    description: "read through next chapter of japanese book",
+    timesReturned: 0,
+    id: 2,
+  },
 ];
 
 db.push("/topics", DEV_TOPICS);
 db.push("/deletedIndecies", []);
 
-test("randomIndex", () => {
+test("randomIndex within topics array", () => {
   const randomInd = randomIndex(db);
   expect(randomInd).toBeLessThan(DEV_TOPICS.length);
 });
+
+describe("app.ts", () => {
+  it('should return list of topics at `/topics`', async() => {
+    const res = await request(app).get('/topics')
+    // console.log(res)
+    expect(res.body).toEqual(DEV_TOPICS)
+  })
+})

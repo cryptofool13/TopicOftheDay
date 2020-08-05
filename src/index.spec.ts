1dec9f4d86dd65b73f2e81c77bd4281fa6bcbd1f
cryptofool13 <fender18295@gmail.com>
2020-08-05 07:27:18 -0400

    crud tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,10 @@
 import { JsonDB } from "node-json-db";
 import { Config } from "node-json-db/dist/lib/JsonDBConfig";
-import  request from 'supertest'
+import request from "supertest";
 
-import {app} from './app'
-import {db} from './db'
+import { app } from "./app";
+import { db } from "./db";
 import { randomIndex, Topic } from "./utils";
-
 
 const DEV_TOPICS: Topic[] = [
   {
@@ -37,9 +36,26 @@ test("randomIndex within topics array", () => {
 });
 
 describe("app.ts", () => {
-  it('should return list of topics at `/topics`', async() => {
-    const res = await request(app).get('/topics')
-    // console.log(res)
-    expect(res.body).toEqual(DEV_TOPICS)
-  })
-})
+  it("should return list of topics at `/topics`", async () => {
+    const res = await request(app).get("/topics");
+    expect(res.body).toEqual(DEV_TOPICS);
+  });
+  it("should return a specific topic", async () => {
+    const res = await request(app).get("/topics/0");
+    expect(res.body).toEqual(DEV_TOPICS[0]);
+  });
+  it("should add a new topic", async () => {
+    const newTopic = {
+      name: "a new topic",
+      description: "new topic description",
+    };
+    const res = await request(app).post("/topic").send({ topic: newTopic });
+    expect(res.body).toHaveLength(4);
+  });
+  it("should delete a topic", async () => {
+    const res = await request(app).delete("/topics/3");
+    expect(res.body).not.toHaveProperty("error");
+    const otherTopics = await request(app).get("/topics");
+    expect(otherTopics.body[3]).toBeNull();
+  });
+});
